85e4590d6d45652c55fb53250a82fec9501b0a10
Пользователь <kogeka0001@gmail.com>
2025-07-25 19:57:35 +0500

    init commit

---
+++ src/main/java/org/example/opencv/OpenCvApplication.java
@@ -0,0 +1,14 @@
+package org.example.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCvApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OpenCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/opencv/OpenCvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.opencv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
